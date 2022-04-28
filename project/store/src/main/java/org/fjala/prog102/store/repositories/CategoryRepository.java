97beeb23ac9aa5a98b25fb60d601541a5a160955
Dru27 <diana.rojas@fundacion-jala.org>
2022-04-28 17:38:47 -0400

    Modified category entity

--- project/store/src/main/java/org/fjala/prog102/store/repositories/CategoryRepository.java
+++ project/store/src/main/java/org/fjala/prog102/store/repositories/CategoryRepository.java
@@ -2,13 +2,12 @@ package org.fjala.prog102.store.repositories;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.fjala.prog102.store.models.Category;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, String> {
+public interface CategoryRepository extends CrudRepository<Category, String> {
 
     public abstract List<Category> findByName(String name);
 
