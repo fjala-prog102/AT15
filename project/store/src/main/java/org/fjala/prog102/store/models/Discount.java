c58e4fd67336302b870fe358df0d4ce4f6933b1b
adhes14 <adhemarduran@gmail.com>
2022-04-29 00:32:26 -0400

    add validations to discount model fields

--- project/store/src/main/java/org/fjala/prog102/store/models/Discount.java
+++ project/store/src/main/java/org/fjala/prog102/store/models/Discount.java
@@ -2,6 +2,11 @@ package org.fjala.prog102.store.models;
 
 import java.sql.Date;
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
@@ -10,8 +15,17 @@ public class Discount {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long discountId;
+
+    @Min(value = 0)
+    @Max(value = 1)
     private double percentage;
+
+    @NotNull
     private Date startDate;
+
+    @NotNull
     private Date endDate;
+
+    @NotEmpty
     private String description;
 }
