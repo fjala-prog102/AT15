19bf351c5558afb3884944247b56416fa1ccc34c
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-05-02 21:31:08 -0400

    Change BrandRepository inheritance to JpaRepository

--- project/store/src/main/java/org/fjala/prog102/store/repositories/BrandRepository.java
+++ project/store/src/main/java/org/fjala/prog102/store/repositories/BrandRepository.java
@@ -1,8 +1,10 @@
 package org.fjala.prog102.store.repositories;
 
+import java.util.List;
 import org.fjala.prog102.store.models.Brand;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BrandRepository extends CrudRepository<Brand, Long> {
-
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+    public abstract Brand findBrandByName(String brandName);
+    public abstract List<Brand> deleteBrandByName(String brandName);
 }
