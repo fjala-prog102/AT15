29673d880370074bc3e477439ffbc32c90b60378
Dru27 <diana.rojas@fundacion-jala.org>
2022-05-03 18:12:52 -0400

    deleted name, table name modified

--- project/store/src/main/java/org/fjala/prog102/store/models/Category.java
+++ project/store/src/main/java/org/fjala/prog102/store/models/Category.java
@@ -8,12 +8,12 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "category")
+@Table(name = "categories")
 public class Category {
     @Id
-    @Column(unique = true, nullable = false, name = "Name")
+    @Column(unique = true, nullable = false)
     private String name;
 
-    @Column(name = "Description", length = 300)
+    @Column(length = 300)
     private String description;
 }
