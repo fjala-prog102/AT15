a45ba3ee37f6fae73b9b34ebd3a4c22afd330cd3
adhemar <adhemarduran@gmail.com>
2022-05-06 08:36:40 -0400

    Test for get discounts

--- project/store/src/test/java/org/fjala/prog102/store/crontrollers/DiscountApplicationTest.java
+++ project/store/src/test/java/org/fjala/prog102/store/crontrollers/DiscountApplicationTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,6 +17,8 @@ public class DiscountApplicationTest {
 
     @Test
     public void itShouldGetDiscounts() throws Exception {
-        this.mockMvc.perform(get("/discounts")).andDo(print()).andExpect(status().isOk());
+        this.mockMvc.perform(get("/discounts"))
+            .andDo(print())
+            .andExpect(status().isOk());
     }
 }
