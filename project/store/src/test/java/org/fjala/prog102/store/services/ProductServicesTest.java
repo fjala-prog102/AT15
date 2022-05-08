87ab3dc9ab37e56476874858f44f6ee900e94a2b
Adrian Oviedo Gamboa <adryoviedo@gmail.com>
2022-05-08 15:52:22 -0400

    added tests to product services

--- project/store/src/test/java/org/fjala/prog102/store/services/ProductServicesTest.java
+++ project/store/src/test/java/org/fjala/prog102/store/services/ProductServicesTest.java
@@ -1,8 +1,10 @@
 package org.fjala.prog102.store.services;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.fjala.prog102.store.models.Brand;
 import org.fjala.prog102.store.models.Product;
@@ -38,5 +40,39 @@ public class ProductServicesTest {
         productServices.saveProduct(product);
         assertNotNull(product);
         assertTrue(product.getCategories() == null || product.getCategories().size() == 0);
+    }
+
+    @Test
+    public void saveProductTestExepction() {
+        Brand brand = new Brand();
+        brand.setName("Sony");
+        brandServices.saveBrand(brand);
+
+        String expectedMessage = "To create a new product, you do not have to set an ID";
+        try {
+            Product product = new Product();
+            product.setProductId(1L);
+            product.setActive(true);
+            product.setPrice(1);
+            product.setBrand(brand);
+            productServices.saveProduct(product);
+        } catch (RuntimeException myrRuntimeException) {
+            assertEquals(expectedMessage, myrRuntimeException.getMessage());
+        }
+    }
+
+    @Test
+    public void getProductByIdTest() {
+        Brand brand = new Brand();
+        brand.setName("Sony");
+        brandServices.saveBrand(brand);
+
+        Product product = new Product();
+        product.setActive(true);
+        product.setPrice(1);
+        product.setBrand(brand);
+        productServices.saveProduct(product);
+        Optional<Product> optional = productServices.getById(product.getProductId());
+        assertNotNull(optional);
     }
 }
