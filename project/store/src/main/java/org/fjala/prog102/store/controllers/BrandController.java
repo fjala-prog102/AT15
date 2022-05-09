fa0cca1d1dcff74992b93023519a0205fdf202ce
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-05-09 17:48:58 -0400

    Improve Http response

--- project/store/src/main/java/org/fjala/prog102/store/controllers/BrandController.java
+++ project/store/src/main/java/org/fjala/prog102/store/controllers/BrandController.java
@@ -2,45 +2,62 @@ package org.fjala.prog102.store.controllers;
 
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+
+import org.fjala.prog102.store.dto.RestResponseDto;
+import org.fjala.prog102.store.exception.ResourceNotFoundException;
 import org.fjala.prog102.store.models.Brand;
 import org.fjala.prog102.store.services.BrandServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/brands")
+@Validated
 public class BrandController {
     @Autowired
     private BrandServices brandServices;
 
     @GetMapping
-    public List<Brand> getBrands() {
-        return brandServices.getBrands();
+    public RestResponseDto<List<Brand>> getBrands() {
+        RestResponseDto<List<Brand>> response = new RestResponseDto<>();
+        response.setData(brandServices.getBrands());
+        return response;
     }
 
     @PostMapping
-    public Brand saveBrand(@RequestBody Brand brand) {
-        return brandServices.saveBrand(brand);
+    @ResponseStatus(HttpStatus.CREATED)
+    public RestResponseDto<Brand> saveBrand(@RequestBody Brand brand) {
+        RestResponseDto<Brand> response = new RestResponseDto<>();
+        response.setData(brandServices.saveBrand(brand));
+        return response;
     }
 
     @GetMapping(path = "/{name}")
-    public Brand findBrandByName(@PathVariable("name") String name) {
-        return brandServices.findBrandByName(name);
+    public RestResponseDto<Brand> findBrandByName(@PathVariable("name") String name) {
+        RestResponseDto<Brand> response = new RestResponseDto<>();
+        response.setData(brandServices.findBrandByName(name));
+        return response;
     }
 
     @DeleteMapping(path = "/{name}")
-    public String deleteBrandByName(@PathVariable("name") String name) {
-        boolean result = brandServices.deleteBrandByName(name);
-        if (result) {
-            return String.format("Brand %s was deleted", name);
-        } else {
-            return String.format("Brand %s was not found", name);
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteBrandByName(@NotBlank(message = "Brand name cannot be blank") @PathVariable("name") String name) {
+        try {
+            brandServices.deleteBrandByName(name);
+        } catch (ResourceNotFoundException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
     }
 }
