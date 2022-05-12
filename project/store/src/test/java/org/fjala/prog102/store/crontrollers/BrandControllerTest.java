da1fe6e7596cd11dba1eca4c4dd9f8685552889f
Saul Fuentes <saul.fuentes@fundacion-jala.org>
2022-05-12 17:58:41 -0400

    Add unit tests for getBrand and saveBrand methods

--- project/store/src/test/java/org/fjala/prog102/store/crontrollers/BrandControllerTest.java
+++ project/store/src/test/java/org/fjala/prog102/store/crontrollers/BrandControllerTest.java
@@ -1,35 +1,55 @@
 package org.fjala.prog102.store.crontrollers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 import org.fjala.prog102.store.models.Brand;
 import org.fjala.prog102.store.services.BrandServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+// import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-
+@SpringBootTest
+@AutoConfigureMockMvc
 public class BrandControllerTest {
-
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private BrandServices brandServices;
 
+    @Test
+    public void itShouldGetBrands() throws Exception{
+        this.mockMvc.perform(get("/brands"))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
 
-    // @Test
-    // public void itShouldGetBrands() throws Exception{
-    //     this.mockMvc.perform(get("/brands"))
-    //         .andDo(print())
-    //         .andExpect(status().isOk());
-    // }
+    @Test
+    public void itShouldCreateABrand() throws Exception {
+        Brand brand = new Brand();
+        brand.setName("Samsung");
+        brand.setDescription("Electronic products");
+        brand.setWebsite("www.samsung.com");
+        brandServices.saveBrand(brand);
+
+        String stringBody = "{";
+        stringBody += "\"name\":\"Samsung\",";
+        stringBody += "\"description\":\"Electronic products\",";
+        stringBody += "\"website\":\"www.samsung.com\"";
+        stringBody += "}";
+
+        this.mockMvc.perform(
+            post("/brands")
+            .content(stringBody)
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isCreated());
+    }
+
 
 }
