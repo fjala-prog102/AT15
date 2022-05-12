7b49c555c93319831f59467105b12c82e9235259
Agustin-Mediotti <agustinmediotti@gmail.com>
2022-05-12 09:21:01 -0300

    added CreateAClientTest

--- project/store/src/test/java/org/fjala/prog102/store/crontrollers/ClientControllerTest.java
+++ project/store/src/test/java/org/fjala/prog102/store/crontrollers/ClientControllerTest.java
@@ -1,14 +1,17 @@
 package org.fjala.prog102.store.crontrollers;
 
+import org.fjala.prog102.store.models.Brand;
 import org.fjala.prog102.store.services.ClientServices;
 import org.fjala.prog102.store.services.ProductServices;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,5 +30,23 @@ public class ClientControllerTest {
         this.mockMvc.perform(get("/clients"))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void itShouldCreateAClient() throws Exception {
+
+        String stringBody = "{";
+        stringBody += "\"identificationNumber\":39509979,";
+        stringBody += "\"firstName\":\"Agustin\",";
+        stringBody += "\"lastName\":\"Mediotti\",";
+        stringBody += "\"address\":\"43, 845 LP\"";
+        stringBody += "}";
+
+        this.mockMvc.perform(
+                        post("/clients")
+                                .content(stringBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
     }
 }
